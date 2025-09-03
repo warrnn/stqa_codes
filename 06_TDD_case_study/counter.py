d96f3a1a696a19a684e80a38d58ee65b5ebc1dd9
Bryan Warren <bryanwarren86@gmail.com>
2025-09-03 15:58:28 +0700

    Test Driven Development Method

--- 06_TDD_case_study/counter.py
+++ 06_TDD_case_study/counter.py
@@ -8,3 +8,42 @@ COUNTERS = {}
 @app.route("/counters/<name>", methods=["POST"])
 def create_counter(name):
     """Creates a counter"""
+    global COUNTERS
+    
+    if name in COUNTERS:
+        return {}, status.HTTP_409_CONFLICT
+    
+    COUNTERS[name] = 1
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+@app.route("/counters/<name>", methods=["PUT"])
+def update_counter(name):
+    """Updates a counter"""
+    global COUNTERS
+    
+    if name not in COUNTERS:
+        return {}, status.HTTP_404_NOT_FOUND
+    
+    COUNTERS[name] += 1
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+@app.route("/counters/<name>", methods=["GET"])
+def read_counter(name):
+    """Reads a counter"""
+    global COUNTERS
+    
+    if name not in COUNTERS:
+        return {}, status.HTTP_404_NOT_FOUND
+    
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counter(name):
+    """Deletes a counter"""
+    global COUNTERS
+    
+    if name not in COUNTERS:
+        return {}, status.HTTP_404_NOT_FOUND
+    
+    del COUNTERS[name]
+    return {}, status.HTTP_200_OK
--- 06_TDD_case_study/test_counter.py
+++ 06_TDD_case_study/test_counter.py
@@ -10,3 +10,58 @@ class CounterTest(TestCase):
 
     def setUp(self):
         self.client = app.test_client()
+        
+    def test_create_counter(self):
+        response = self.client.post("/counters/test-counter")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        data = response.get_json()
+        self.assertIn("test-counter", data)
+        self.assertEqual(data["test-counter"], 1)
+
+    def test_create_duplicate_counter(self):
+        response = self.client.post("/counters/test-counter-1")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post("/counters/test-counter-1")
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
+        
+    def test_update_counter(self):
+        response = self.client.post("/counters/test-counter-2")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.put("/counters/test-counter-2")  
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        
+        data = response.get_json()
+        self.assertIn("test-counter-2", data)
+        self.assertEqual(data["test-counter-2"], 2)
+        
+    def test_update_counter_failed(self):
+        response = self.client.put("/counters/test-counter-3")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
+    def test_read_counter(self):
+        response = self.client.post("/counters/test-counter-4")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.get("/counters/test-counter-4")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        data = response.get_json()
+        self.assertIn("test-counter-4", data)
+        self.assertEqual(data["test-counter-4"], 1)
+        
+    def test_read_counter_failed(self):
+        response = self.client.get("/counters/test-counter-5")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
+    def test_delete_counter(self):
+        response = self.client.post("/counters/test-counter-6")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.delete("/counters/test-counter-6")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        data = response.get_json()
+        self.assertEqual(data, {})
+        
+    def test_delete_counter_failed(self):
+        response = self.client.delete("/counters/test-counter-7")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
